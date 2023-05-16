f721b66bf93731988a898e7070fab49df08b8a83
Roberto Guerra <prosaka@hotmail.com>
2023-05-16 20:20:18 -0300

    feat: creat signin page with fields

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,39 @@
-import { Text } from "react-native";
+import { Input } from "@components/Input";
+import {
+  BoxInput,
+  Container,
+  SignUpButton,
+  SignUpText,
+  Title,
+  Underline,
+} from "./styles";
+import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export const SignIn = () => {
-  return <Text>SignIn</Text>;
+  const navigation = useNavigation();
+
+  function handleSignUp() {
+    navigation.navigate("SignUp");
+  }
+
+  return (
+    <Container>
+      <Title>Login</Title>
+      <BoxInput>
+        <Input name="E-mail" placeholder="email@email.com" />
+        <Input
+          name="Password"
+          placeholder="Minimum 8 characters"
+          secureTextEntry
+        />
+      </BoxInput>
+      <Button>Login</Button>
+      <SignUpButton onPress={handleSignUp}>
+        <SignUpText>
+          Don’t have an account? <Underline>Sign up</Underline> here
+        </SignUpText>
+      </SignUpButton>
+    </Container>
+  );
 };
